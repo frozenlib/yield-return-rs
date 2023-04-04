4a08a70e6a228d9df998d508195e46318ea27dd9
frozenlib <frozenlib@users.noreply.github.com>
2023-04-04 09:53:23 +0900

    The initial experimental implementation.

---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+use std::{
+    cell::RefCell,
+    future::Future,
+    pin::Pin,
+    rc::Rc,
+    sync::Arc,
+    task::{
+        Context,
+        Poll::{self, Pending, Ready},
+        Wake, Waker,
+    },
+};
+
+struct ValueStore<T>(Rc<RefCell<Option<T>>>);
+
+impl<T> ValueStore<T> {
+    #[track_caller]
+    fn set(&self, value: T) {
+        let mut b = self.0.borrow_mut();
+        assert!(
+            b.is_none(),
+            "The result of the previous `ret` is not await."
+        );
+        *b = Some(value);
+    }
+}
+impl<T> Future for ValueStore<T> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
+        if self.0.borrow().is_some() {
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+}
+
+pub struct YieldContext<T>(ValueStore<T>);
+
+impl<T> YieldContext<T> {
+    #[track_caller]
+    pub fn ret(&mut self, value: T) -> impl Future<Output = ()> + '_ {
+        self.0.set(value);
+        &mut self.0
+    }
+}
+
+struct RawIter<'a, T> {
+    value: Rc<RefCell<Option<T>>>,
+    fut: Pin<Box<dyn Future<Output = ()> + 'a>>,
+    waker: Waker,
+}
+
+pub struct Yield<'a, T>(Option<RawIter<'a, T>>);
+
+impl<'a, T> Yield<'a, T> {
+    pub fn new<Fut: Future<Output = ()> + 'a>(f: impl FnOnce(YieldContext<T>) -> Fut) -> Self {
+        let value = Rc::new(RefCell::new(None));
+        let cx = YieldContext(ValueStore(value.clone()));
+        let fut = Box::pin(f(cx));
+        let waker = Arc::new(FakeWake).into();
+        Self(Some(RawIter { value, fut, waker }))
+    }
+}
+impl<T> Iterator for Yield<'_, T> {
+    type Item = T;
+    #[track_caller]
+    fn next(&mut self) -> Option<Self::Item> {
+        let raw = self.0.as_mut()?;
+        let poll = raw.fut.as_mut().poll(&mut Context::from_waker(&raw.waker));
+        match poll {
+            Ready(_) => {
+                assert!(
+                    raw.value.borrow().is_none(),
+                    "The result of `ret` is not await."
+                );
+                self.0 = None;
+                None
+            }
+            Pending => {
+                if let Some(value) = raw.value.borrow_mut().take() {
+                    Some(value)
+                } else {
+                    panic!("`YieldContext::ret` is not called.");
+                }
+            }
+        }
+    }
+}
+struct FakeWake;
+impl Wake for FakeWake {
+    fn wake(self: Arc<Self>) {}
+    fn wake_by_ref(self: &Arc<Self>) {}
+}
---
+++ tests/tests.rs
@@ -0,0 +1,50 @@
+use std::future::pending;
+
+use yield_return::Yield;
+
+#[test]
+fn no_value() {
+    let iter = Yield::<u32>::new(|mut _y| async move {});
+    let list: Vec<_> = iter.collect();
+    assert_eq!(list, vec![]);
+}
+
+#[test]
+fn values() {
+    let iter = Yield::new(|mut y| async move {
+        y.ret(1).await;
+        y.ret(2).await;
+    });
+    let list: Vec<_> = iter.collect();
+    assert_eq!(list, vec![1, 2]);
+}
+
+#[test]
+#[allow(unused_must_use)]
+#[should_panic]
+fn no_await_1() {
+    let iter = Yield::new(|mut y| async move {
+        y.ret(1);
+    });
+    let _: Vec<_> = iter.collect();
+}
+
+#[test]
+#[allow(unused_must_use)]
+#[should_panic]
+fn no_await_2() {
+    let iter = Yield::new(|mut y| async move {
+        y.ret(1);
+        y.ret(2);
+    });
+    let _: Vec<_> = iter.collect();
+}
+
+#[test]
+#[should_panic]
+fn invalid_await() {
+    let iter = Yield::<u32>::new(|_y| async move {
+        pending::<()>().await;
+    });
+    let _: Vec<_> = iter.collect();
+}
