2cc7bd90dc6950f78598fccd194821997ffac6a3
frozenlib <frozenlib@users.noreply.github.com>
2025-01-10 10:10:20 +0900

    Rename `Yield` to `LocalIter`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,13 @@
 //! This crate provides a way to implement something like C#'s `yield return` using an asynchronous function.
 //!
-//! See [`Yield::new`] for details.
-use std::{
-    cell::RefCell,
-    future::Future,
-    iter::FusedIterator,
-    pin::Pin,
-    rc::Rc,
-    sync::{Arc, OnceLock},
-    task::{
-        Context,
-        Poll::{self, Pending, Ready},
-        Wake, Waker,
-    },
-};
+//! See [`LocalIter::new`] for details.
 
-struct ValueStore<T>(Rc<RefCell<Option<T>>>);
+mod local_iter;
 
-impl<T> ValueStore<T> {
-    #[track_caller]
-    fn set(&self, value: T) {
-        let mut b = self.0.borrow_mut();
-        assert!(b.is_none(), "The result of `ret` is not await.");
-        *b = Some(value);
-    }
-}
-impl<T> Future for ValueStore<T> {
-    type Output = ();
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<Self::Output> {
-        if self.0.borrow().is_some() {
-            Poll::Pending
-        } else {
-            Poll::Ready(())
-        }
-    }
-}
+pub use local_iter::{LocalIter, LocalIterContext};
 
-/// A context for asynchronous function to implement iterator.
-pub struct YieldContext<T>(ValueStore<T>);
+#[deprecated(since = "0.2.0", note = "Use `LocalIter` instead.")]
+pub type Yield<'a, T> = LocalIter<'a, T>;
 
-impl<T> YieldContext<T> {
-    /// Yields a single value. Similar to C#'s `yield return` or Python's `yield`.
-    #[track_caller]
-    pub fn ret(&mut self, value: T) -> impl Future<Output = ()> + '_ {
-        self.0.set(value);
-        &mut self.0
-    }
-
-    /// Yields all values from an iterator. Similar to Python's `yield from` or JavaScript's `yield*`.
-    pub async fn ret_iter(&mut self, iter: impl IntoIterator<Item = T>) {
-        for value in iter {
-            self.ret(value).await;
-        }
-    }
-}
-
-struct RawYield<'a, T> {
-    value: Rc<RefCell<Option<T>>>,
-    fut: Pin<Box<dyn Future<Output = ()> + 'a>>,
-    waker: Waker,
-}
-
-/// An iterator implemented by asynchronous function.
-pub struct Yield<'a, T>(Option<RawYield<'a, T>>);
-
-impl<'a, T: 'a> Yield<'a, T> {
-    /// Create an iterator from an asynchronous function.
-    ///
-    /// # Example
-    /// ```
-    /// use yield_return::Yield;
-    /// let iter = Yield::new(|mut y| async move {
-    ///     y.ret(1).await;
-    ///     y.ret(2).await;
-    /// });
-    /// let list: Vec<_> = iter.collect();
-    /// assert_eq!(list, vec![1, 2]);
-    /// ```
-    pub fn new<Fut: Future<Output = ()> + 'a>(f: impl FnOnce(YieldContext<T>) -> Fut) -> Self {
-        let value = Rc::new(RefCell::new(None));
-        let cx = YieldContext(ValueStore(value.clone()));
-        let fut = Box::pin(f(cx));
-        static WAKER: OnceLock<Arc<FakeWake>> = OnceLock::new();
-        let waker = WAKER.get_or_init(|| Arc::new(FakeWake)).clone().into();
-        Self(Some(RawYield { value, fut, waker }))
-    }
-}
-impl<T> Iterator for Yield<'_, T> {
-    type Item = T;
-    #[track_caller]
-    fn next(&mut self) -> Option<Self::Item> {
-        let raw = self.0.as_mut()?;
-        let poll = raw.fut.as_mut().poll(&mut Context::from_waker(&raw.waker));
-        match poll {
-            Ready(_) => {
-                assert!(
-                    raw.value.borrow().is_none(),
-                    "The result of `ret` is not await."
-                );
-                self.0 = None;
-                None
-            }
-            Pending => {
-                if let Some(value) = raw.value.borrow_mut().take() {
-                    Some(value)
-                } else {
-                    panic!("`YieldContext::ret` is not called.");
-                }
-            }
-        }
-    }
-}
-impl<T> FusedIterator for Yield<'_, T> {}
-struct FakeWake;
-impl Wake for FakeWake {
-    fn wake(self: Arc<Self>) {}
-    fn wake_by_ref(self: &Arc<Self>) {}
-}
+#[deprecated(since = "0.2.0", note = "Use `LocalIterContext` instead.")]
+pub type YieldContext<T> = LocalIterContext<T>;
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,17 +1,17 @@
 use std::future::pending;
 
-use yield_return::Yield;
+use yield_return::LocalIter;
 
 #[test]
 fn no_value() {
-    let iter = Yield::<u32>::new(|mut _y| async move {});
+    let iter = LocalIter::<u32>::new(|mut _y| async move {});
     let list: Vec<_> = iter.collect();
     assert_eq!(list, vec![]);
 }
 
 #[test]
 fn values() {
-    let iter = Yield::new(|mut y| async move {
+    let iter = LocalIter::new(|mut y| async move {
         y.ret(1).await;
         y.ret(2).await;
     });
@@ -21,7 +21,7 @@ fn values() {
 
 #[test]
 fn values_ret_iter() {
-    let iter = Yield::new(|mut y| async move {
+    let iter = LocalIter::new(|mut y| async move {
         y.ret_iter([1, 2]).await;
     });
     let list: Vec<_> = iter.collect();
@@ -30,7 +30,7 @@ fn values_ret_iter() {
 
 #[test]
 fn fused() {
-    let mut iter = Yield::new(|mut y| async move {
+    let mut iter = LocalIter::new(|mut y| async move {
         y.ret(1).await;
         y.ret(2).await;
     });
@@ -44,7 +44,7 @@ fn fused() {
 fn values_with_lifetime() {
     let items = vec![1, 2];
     let items = &items;
-    let iter = Yield::new(|mut y| async move {
+    let iter = LocalIter::new(|mut y| async move {
         y.ret(&items[0]).await;
         y.ret(&items[1]).await;
     });
@@ -56,7 +56,7 @@ fn values_with_lifetime() {
 #[allow(unused_must_use)]
 #[should_panic]
 fn no_await_1() {
-    let iter = Yield::new(|mut y| async move {
+    let iter = LocalIter::new(|mut y| async move {
         y.ret(1);
     });
     let _: Vec<_> = iter.collect();
@@ -66,7 +66,7 @@ fn no_await_1() {
 #[allow(unused_must_use)]
 #[should_panic]
 fn no_await_2() {
-    let iter = Yield::new(|mut y| async move {
+    let iter = LocalIter::new(|mut y| async move {
         y.ret(1);
         y.ret(2);
     });
@@ -76,7 +76,7 @@ fn no_await_2() {
 #[test]
 #[should_panic]
 fn invalid_await() {
-    let iter = Yield::<u32>::new(|_y| async move {
+    let iter = LocalIter::<u32>::new(|_y| async move {
         pending::<()>().await;
     });
     let _: Vec<_> = iter.collect();
